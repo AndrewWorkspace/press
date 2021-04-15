fa9e2c6fece288e8cf558046485f5e3844a2f67b
AndrewWorkspace <40171574+AndrewWorkspace@users.noreply.github.com>
2021-04-15 21:45:00 +0300

    Bug Fix...

--- js/inits.js
+++ js/inits.js
@@ -57,14 +57,12 @@
 	});
 	
 	var swiper = new Swiper('.swiper-popular-news', {
-		spaceBetween: -20,
-		freeMode: true,
 		slidesPerView: 1,
-		autoplay: {
-			delay: 4700,
-			disableOnInteraction: false,
-		},
-		loop: false,
+		// autoplay: {
+		// 	delay: 4700,
+		// 	disableOnInteraction: false,
+		// },
+		loop: true,
 		navigation: {
 			nextEl: '.slider-nav-next',
 			prevEl: '.slider-nav-prev',
@@ -75,7 +73,75 @@
 				slidesPerView: 1,
 				centeredSlides: true,
 			},
-			576: {
+			340: {
+				spaceBetween: -40,
+				
+			},
+			350: {
+				spaceBetween: -50,
+				
+			},
+			360: {
+				spaceBetween: -60,
+				
+			},
+			370: {
+				spaceBetween: -70,
+				
+			},
+			380: {
+				spaceBetween: -80,
+				
+			},
+			400: {
+				spaceBetween: -100,
+				
+			},
+			410: {
+				spaceBetween: -110,
+				
+			},
+			420: {
+				spaceBetween: -120,
+				
+			},
+			430: {
+				spaceBetween: -130,
+				
+			},
+			440: {
+				spaceBetween: -140,
+				
+			},
+			450: {
+				spaceBetween: -150,
+				
+			},
+			460: {
+				spaceBetween: -160,
+				
+			},
+			470: {
+				spaceBetween: -170,
+				
+			},
+			480: {
+				spaceBetween: -180,
+				
+			},
+			490: {
+				spaceBetween: -190,
+				
+			},
+			500: {
+				spaceBetween: -200,
+				
+			},
+			510: {
+				spaceBetween: -210,
+				
+			},
+			520: {
 				slidesPerView: 2,
 				spaceBetween: 20,
 				centeredSlides: false,
@@ -109,17 +175,85 @@
 		},
 		breakpoints: {
 			320: {
-				spaceBetween: 10,
+				spaceBetween: 0,
 				slidesPerView: 1,
 				centeredSlides: true,
 			},
-			576: {
+			340: {
+				spaceBetween: -10,
+				
+			},
+			350: {
+				spaceBetween: -20,
+				
+			},
+			360: {
+				spaceBetween: -30,
+				
+			},
+			370: {
+				spaceBetween: -40,
+				
+			},
+			380: {
+				spaceBetween: -50,
+				
+			},
+			400: {
+				spaceBetween: -60,
+				
+			},
+			410: {
+				spaceBetween: -70,
+				
+			},
+			420: {
+				spaceBetween: -80,
+				
+			},
+			430: {
+				spaceBetween: -90,
+				
+			},
+			440: {
+				spaceBetween: -100,
+				
+			},
+			450: {
+				spaceBetween: -120,
+				
+			},
+			460: {
+				spaceBetween: -130,
+				
+			},
+			470: {
+				spaceBetween: -140,
+				
+			},
+			480: {
+				spaceBetween: -150,
+				
+			},
+			490: {
+				spaceBetween: -160,
+				
+			},
+			500: {
+				spaceBetween: -170,
+				
+			},
+			510: {
+				spaceBetween: -180,
+				
+			},
+			520: {
 				slidesPerView: 2,
 				spaceBetween: 20,
 				centeredSlides: false,
 			},
 			768: {
-				slidesPerView: 3,
+				slidesPerView: 2,
 			},
 			992: {
 				slidesPerView: 3,
@@ -133,7 +267,6 @@
 	
 	
 	var swiperNewArticles = new Swiper('.swiper-new-articles', {
-		spaceBetween: 20,
 		freeMode: true,
 		slidesPerView: 1,
 		autoplay: {
@@ -151,7 +284,75 @@
 				slidesPerView: 1,
 				centeredSlides: true,
 			},
-			576: {
+			340: {
+				spaceBetween: -40,
+				
+			},
+			350: {
+				spaceBetween: -50,
+				
+			},
+			360: {
+				spaceBetween: -60,
+				
+			},
+			370: {
+				spaceBetween: -70,
+				
+			},
+			380: {
+				spaceBetween: -80,
+				
+			},
+			400: {
+				spaceBetween: -100,
+				
+			},
+			410: {
+				spaceBetween: -110,
+				
+			},
+			420: {
+				spaceBetween: -120,
+				
+			},
+			430: {
+				spaceBetween: -130,
+				
+			},
+			440: {
+				spaceBetween: -140,
+				
+			},
+			450: {
+				spaceBetween: -150,
+				
+			},
+			460: {
+				spaceBetween: -160,
+				
+			},
+			470: {
+				spaceBetween: -170,
+				
+			},
+			480: {
+				spaceBetween: -180,
+				
+			},
+			490: {
+				spaceBetween: -190,
+				
+			},
+			500: {
+				spaceBetween: -200,
+				
+			},
+			510: {
+				spaceBetween: -210,
+				
+			},
+			520: {
 				slidesPerView: 2,
 				spaceBetween: 20,
 				centeredSlides: false,
